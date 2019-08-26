9635e31750754f73cad2bdffdf04f5a3dddf5794
Victor <contato@victorshinya.com.br>
2019-08-26 13:10:43 -0300

    Update to Assistant v2

--- assistant.js
+++ assistant.js
@@ -6,27 +6,41 @@
  * @param {Object} context The context managed by Watson
  * @returns {Object} Returns the complete object
  */
-const WatsonAssistantV1 = require('ibm-watson/assistant/v1')
-const assistant = new WatsonAssistantV1({
+const WatsonAssistantV2 = require('ibm-watson/assistant/v2')
+const assistant = new WatsonAssistantV2({
     version: '2019-02-28',
     iam_apikey: '{iam_apikey}',
-    url: 'https://gateway.watsonplatform.net/assistant/api'
+    url: 'https://gateway.watsonplatform.net/assistant/api',
 })
 
-function main(params) {
-    return new Promise((resolve, reject) => {
-        assistant.message({
-            input: params.input,
-            context: params.context,
-            workspace_id: '{workspace_id}'
-        }, (err, response) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(response)
-            }
+async function main(params) {
+    try {
+        if (!params.session_id) {
+            const sessionResponse = await assistant.createSession({
+                assistant_id: '{assistant_id}'
+            })
+            console.log(`session_id: ${sessionResponse.session_id}`)
+            params.session_id = sessionResponse.session_id
+        }
+        console.log(`params: ${JSON.stringify(params)}`)
+        const response = await assistant.message({
+            assistant_id: '{assistant_id}',
+            session_id: params.session_id,
+            input: {
+                text: params.input.text,
+                options: {
+                    return_context: true,
+                }
+            },
+            context: params.context
         })
-    })
+        response.session_id = params.session_id
+        return response
+    }
+    catch(err) {
+        console.error(`error: ${err.message}`)
+        return err
+    }
 }
 
 exports.main = main
